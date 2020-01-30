cc4d48e631091feeca1b8c6c08fc87a8f2b52fa1
molopezCU <molopez@chapman.edu>
2020-01-30 04:47:00 +0000

    Problem 1 with style guide.

--- Problem2/Problem2.cpp
+++ Problem2/Problem2.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 
+using namespace std;
 int main(int argc, char **argv){
-  public:
-    int roomCapacity = 39;
-    int roomAttendance;
-    int spotsRemaining
-  private:
-    //none
 
+  //member variables
+    unsigned int roomCapacity;
+    unsigned int roomAttendance;
+    unsigned int spotsRemaining;
+
+    cout << "What is the room capacity?: " << endl;
+    cin >> roomCapacity;
     cout << "How many people are in the room?: " << endl;
     cin >> roomAttendance;
 
-    if(roomCapacity < 39){
-
+    if(roomAttendance > roomCapacity){
+       cout << "The meeting cannot be held as planned due to fire regulations." << endl;
+       spotsRemaining = roomAttendance - roomCapacity;
+       cout << spotsRemaining << ": person(s) must be excluded. And no they do not get paid." << endl;
+    } else if (roomAttendance == roomCapacity){
+      cout << "Maximum room capacity." << endl;
+    } else {
+      cout << "The meeting is withing fire safety laws. Proceed as planned." << endl;
+      spotsRemaining = roomCapacity - roomAttendance;
+      cout << spotsRemaining << ": person(s) can be addded to the meeting before violating fire safety law." << endl;
     }
+
+  return 0;
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CPSC298_Assignment1
+Moises Lopez
+2297564
+molopez@chapman.edu
+CPSC 298-01
+Assignment 1
+
+README.md
